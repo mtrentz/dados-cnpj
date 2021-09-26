ab2196951d2a32c683c02c57574070f0fcf6287e
mateus trentz <mateus.trentz@gmail.com>
2021-09-26 15:51:47 -0300

    testing out yet

--- main.go
+++ main.go
@@ -1,13 +1,26 @@
 package main
 
-import "github.com/mtrentz/dados-cnpj/transform"
+import (
+	"github.com/mtrentz/dados-cnpj/transform"
+)
 
 func main() {
-	// dataDir := "data"
-	// 	OrganizedFiles := organize.GetFiles(dataDir)
-	// 	// transform.Unzip()
-	// 	transform.Unzip(&OrganizedFiles, dataDir)
+	dataDir := "data"
+	// OrganizedFiles := organize.GetFiles(dataDir)
+	// for i, e := range OrganizedFiles {
+	// 	fmt.Println(i, ":")
+	// 	for _, val := range e {
+	// 		fmt.Println(">>>", val)
+	// 	}
+	// }
+
+	// transform.Unzip(OrganizedFiles, dataDir)
+
+	transform.ConcatAll(dataDir)
+
+	// transform.AppendAllLines("data/socio/socios.csv", "data/socio/K3241.K03200Y0.D10911.SOCIOCSV", false)
+	// transform.AppendAllLines("data/socio/socios.csv", "data/socio/K3241.K03200Y1.D10911.SOCIOCSV", false)
 
 	// transform.GetFilesByCategory(dataDir)
-	transform.ConcatCsvs()
+	// transform.ConcatCsvs()
 }
