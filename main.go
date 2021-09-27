c22877d3ce455bc6140c86c8a26cb8306f074f83
mateus trentz <mateus.trentz@gmail.com>
2021-09-27 17:34:47 -0300

    tets

--- main.go
+++ main.go
@@ -1,33 +1,30 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	"log"
-
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/mtrentz/dados-cnpj/database"
+	"github.com/mtrentz/dados-cnpj/organize"
+	"github.com/mtrentz/dados-cnpj/transform"
 )
 
 func main() {
 
-	// Connect to database, set it to global variable
-	fmt.Println("Connecting to database...")
-	var err error
-	database.DB, err = sql.Open("mysql", "root:999999@tcp(127.0.0.1:3366)/dados_cnpj?charset=latin2&collation=latin2_general_ci&autocommit=false")
-	if err != nil {
-		log.Fatal("Unable to open connection to db: ", err)
-	}
-	defer database.DB.Close()
+	// // Connect to database, set it to global variable
+	// fmt.Println("Connecting to database...")
+	// var err error
+	// database.DB, err = sql.Open("mysql", "root:999999@tcp(127.0.0.1:3366)/dados_cnpj?charset=latin2&collation=latin2_general_ci&autocommit=false")
+	// if err != nil {
+	// 	log.Fatal("Unable to open connection to db: ", err)
+	// }
+	// defer database.DB.Close()
 
-	// dataDir := "data"
-	// OrganizedFiles := organize.GetFiles(dataDir)
+	dataDir := "data"
+	OrganizedFiles := organize.GetFiles(dataDir)
 
-	// transform.Unzip(OrganizedFiles, dataDir)
+	transform.Unzip(OrganizedFiles, dataDir)
 
-	// transform.ConcatAll(dataDir)
+	transform.ConcatAll(dataDir)
 
-	database.InsertAll()
+	// database.InsertAll()
 
 	// transform.AppendAllLines("data/socio/socios.csv", "data/socio/K3241.K03200Y0.D10911.SOCIOCSV", false)
 	// transform.AppendAllLines("data/socio/socios.csv", "data/socio/K3241.K03200Y1.D10911.SOCIOCSV", false)
