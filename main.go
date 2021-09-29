ae3b4e420f57735654e3aa3e741326b3c1cfb337
mateus trentz <mateus.trentz@gmail.com>
2021-09-29 19:42:07 -0300

    cli

--- main.go
+++ main.go
@@ -1,28 +1,7 @@
 package main
 
-import (
-	"github.com/mtrentz/dados-cnpj/database"
-	"github.com/mtrentz/dados-cnpj/download"
-)
+import "github.com/mtrentz/dados-cnpj/cmd"
 
 func main() {
-
-	database.Connect()
-
-	dataDir := "data"
-	download.DownloadAll(dataDir)
-	// OrganizedFiles := organize.GetFiles(dataDir)
-
-	// transform.Unzip(OrganizedFiles, dataDir)
-
-	// transform.ConcatAll(dataDir)
-
-	// database.InsertAll()
-	// database.TestInsert()
-
-	// transform.AppendAllLines("data/socio/socios.csv", "data/socio/K3241.K03200Y0.D10911.SOCIOCSV", false)
-	// transform.AppendAllLines("data/socio/socios.csv", "data/socio/K3241.K03200Y1.D10911.SOCIOCSV", false)
-
-	// transform.GetFilesByCategory(dataDir)
-	// transform.ConcatCsvs()
+	cmd.Execute()
 }
