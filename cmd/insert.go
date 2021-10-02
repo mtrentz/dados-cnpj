cff0b7ccbabf2957f7e373866acac9a0b7e3d4bf
mateus trentz <mateus.trentz@gmail.com>
2021-10-02 20:40:20 -0300

    descricoes cli

--- cmd/insert.go
+++ cmd/insert.go
@@ -1,18 +1,3 @@
-/*
-Copyright © 2021 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
@@ -23,13 +8,17 @@ import (
 // insertCmd represents the insert command
 var insertCmd = &cobra.Command{
 	Use:   "insert",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Insere os arquivos csvs concatenados ao database",
+	Long: `Insere os arquivos csvs de cada categoria já concatenados e localizados dentro
+da pasta data/categoria ao banco de dados.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+É necessário manter a estrutura dos diretórios como é criado pelo comando transform. Ou seja,
+dentro da pata data deve haver outras pastas com os nome das categorias dos arquivos: cnaes, empresas, etc...
+
+Dentro dessas pastas deve haver um único csv que será inserido ao banco.
+
+As configurações do banco de dados ficam em database/database.go. Neste arquivo é possível
+mudar o local do banco de destino caso seja necessário.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		database.Connect()
 		database.InsertAll()
@@ -38,14 +27,4 @@ to quickly create a Cobra application.`,
 
 func init() {
 	rootCmd.AddCommand(insertCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// insertCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// insertCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- cmd/transform.go
+++ cmd/transform.go
@@ -1,18 +1,3 @@
-/*
-Copyright © 2021 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
@@ -24,13 +9,13 @@ import (
 // transformCmd represents the transform command
 var transformCmd = &cobra.Command{
 	Use:   "transform",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Unzip todos os arquivos baixados da receita federal. Concatena todos os csvs em um só arquivo.",
+	Long: `Tendo todos os arquivos zips da receita federal baixados na pasta data, unzipa todos eles
+para uma pasta com o nome de sua categoria (empresa, cnae, etc...). Como a receita providencia vários csvs,
+aqui também todos estes arquivos são concatenados em um único csv para cada categoria.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+Para garantir o bom funcionamento dessa etapa remova todas as pastas dentro da pasta data e coloque todos os
+arquivos zip com o mesmo nome dos arquivos baixados do site da receita federal.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		OrganizedFiles := organize.GetFiles("data")
 		transform.UnzipAll(OrganizedFiles, "data")
@@ -40,14 +25,4 @@ to quickly create a Cobra application.`,
 
 func init() {
 	rootCmd.AddCommand(transformCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// transformCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// transformCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
